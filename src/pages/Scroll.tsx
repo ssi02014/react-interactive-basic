f71bab9c3594fd199f04c70d77d65c8bd6582137
ssi02014 <ssi02014@naver.com>
2023-06-09 00:56:52 +0900

    feat: scroll 페이지 구현

--- src/pages/Scroll.tsx
+++ src/pages/Scroll.tsx
@@ -1,21 +1,30 @@
 import { useEffect, useState } from "react";
 import { styled } from "styled-components";
+import crabImg from "../assets/scroll/crab.png";
+import cuttleImg from "../assets/scroll/cuttle.png";
+import fishImg from "../assets/scroll/fish_01.png";
+import octoImg from "../assets/scroll/octo.png";
+import seaweedImg from "../assets/scroll/seaweed.png";
+import submarineImg from "../assets/scroll/submarine.gif";
 
 const Scroll = () => {
-  const [scrollNum, setScrollNum] = useState(0);
+  const [percent, setPercent] = useState(0);
+  const [scrollHeight, setScrollHeight] = useState(0);
 
-  const getPercent = (num: number, totalNum: number) => {
+  const getCalculatedPercent = (num: number, totalNum: number) => {
     return +((num / totalNum) * 100).toFixed(0);
   };
 
   useEffect(() => {
     const scroll = () => {
-      const percent = getPercent(
+      // 퍼센트 계산
+      const calculatedPercent = getCalculatedPercent(
         window.scrollY,
         document.body.scrollHeight - window.innerHeight
       );
 
-      setScrollNum(percent);
+      setPercent(calculatedPercent);
+      setScrollHeight(+window.scrollY.toFixed(0));
     };
 
     window.addEventListener("scroll", scroll);
@@ -25,10 +34,36 @@ const Scroll = () => {
   return (
     <Wrapper>
       <ProgressWrap>
-        <ProgressBar scrollNum={scrollNum} />
+        <ProgressBar percent={percent} />
       </ProgressWrap>
 
-      <h1>{scrollNum}%</h1>
+      <MarineImage src={submarineImg} percent={percent} />
+
+      <InnerWrap>
+        <h2>깊은 바다</h2>
+        <TextWrap>
+          <p>대게</p>
+          <img src={crabImg} />
+        </TextWrap>
+        <TextWrap>
+          <p>오징어</p>
+          <img src={cuttleImg} />
+        </TextWrap>
+        <TextWrap>
+          <p>방어</p>
+          <img src={fishImg} />
+        </TextWrap>
+        <TextWrap>
+          <p>미역</p>
+          <img src={seaweedImg} />
+        </TextWrap>
+      </InnerWrap>
+
+      <h1>
+        <span>{scrollHeight}</span> meters deep
+      </h1>
+
+      <OctoImage src={octoImg} percent={percent} />
     </Wrapper>
   );
 };
@@ -37,27 +72,49 @@ export default Scroll;
 
 // style
 const Wrapper = styled.div`
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+
   position: relative;
+  overflow-x: hidden;
   width: 100%;
-  height: 5000px;
   background: linear-gradient(
     150deg,
     #13f0cf,
-    #ddf311,
-    #2d9c9a,
+    #6d017d,
+    #220340,
     #220340,
     #220340,
     black
   );
 
-  h1 {
+  & > h1 {
     position: fixed;
-    bottom: 40px;
-    left: 40px;
+    bottom: 50px;
     padding: 20px;
-    font-size: 60px;
+    font-size: 40px;
     color: #fff;
+    text-align: center;
+    width: 100%;
+    padding-bottom: 10px;
+    border-bottom: 2px dashed #fff;
+    font-weight: bold;
+
+    & > span {
+      width: 120px;
+      display: inline-block;
+    }
   }
+`;
+
+const MarineImage = styled.img<{ percent: number }>`
+  position: fixed;
+  width: 200px;
+  top: 20%;
+  transform: ${({ percent }) => `translateX(${percent * 1.5}%)`};
+  transition: transform 0.3s ease-out;
+  z-index: 200;
 `;
 
 const ProgressWrap = styled.div`
@@ -69,11 +126,50 @@ const ProgressWrap = styled.div`
   background-color: #2d9c9a;
 `;
 
-const ProgressBar = styled.div<{ scrollNum: number }>`
-  width: ${({ scrollNum }) => scrollNum}%;
+const ProgressBar = styled.div<{ percent: number }>`
+  width: ${({ percent }) => percent}%;
   height: inherit;
   position: absolute;
   z-index: 100;
   background-color: #fff;
   transition: width 0.2s ease-out;
+`;
+
+const TextWrap = styled.div`
+  padding: 500px 20px;
+
+  & > p {
+    color: #fff;
+    font-size: 30px;
+    text-align: center;
+  }
+
+  & > img {
+    width: 100%;
+  }
+`;
+
+const InnerWrap = styled.div`
+  position: relative;
+  padding: 40px;
+  width: 100%;
+  z-index: 100;
+
+  & > h2 {
+    text-align: center;
+    font-weight: bold;
+    font-size: 50px;
+    margin-top: 100px;
+  }
+`;
+
+const OctoImage = styled.img<{ percent: number }>`
+  position: fixed;
+  bottom: -300px;
+  right: 0;
+  width: 300px;
+  opacity: 0.4;
+  z-index: 1;
+  transform: ${({ percent }) => `translateY(${-percent / 2}%)`};
+  transition: transform 1s ease-out;
 `;
